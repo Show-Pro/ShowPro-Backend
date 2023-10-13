f5cde6bacc8ff05820beda75a805612fb408f8fe
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-10-13 23:44:58 +0700

    feat: working socket.io

--- app.mjs
+++ app.mjs
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv';
-import fetch from 'node-fetch';
-
 import { Server } from 'socket.io';
 
 const io = new Server(8080, {
@@ -12,8 +9,8 @@ const io = new Server(8080, {
 });
 
 io.on('connection', (socket) => {
-	socket.on('product', (arg) => {
-		console.log('received product: ' + arg);
+	socket.on('order', (arg) => {
+		console.log('received product: ' + JSON.stringify(arg));
         io.emit('order', arg);
 	});
 
